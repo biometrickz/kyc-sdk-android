125bbfa16c4e32cbffd0886b672d87a4e91cbd4e
Azat <azatkamzanov@gmail.com>
2023-03-15 17:41:26 +0600

    Новая доработка: Блокировка экрана при скриншоте и при записи экрана, нахождение эмулятора и выход из страницы

--- src/main/java/kz/test/biometric/BiometricDialog.kt
+++ src/main/java/kz/test/biometric/BiometricDialog.kt
@@ -2,20 +2,19 @@ package kz.test.biometric
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.content.Context
 import android.content.Context.CAMERA_SERVICE
 import android.content.pm.PackageManager
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
-import android.hardware.Camera
 import android.hardware.camera2.CameraCharacteristics
 import android.hardware.camera2.CameraManager
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
+import android.view.WindowManager
 import android.webkit.*
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
@@ -23,53 +22,20 @@ import androidx.core.os.bundleOf
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.LifecycleOwner
-import kz.test.biometric.databinding.DialogBiometricBinding
-import org.json.JSONObject
+import androidx.lifecycle.lifecycleScope
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
-
-
-//private const val BASE_URL = "https://test.biometric.kz/document?"
-private const val BASE_URL = "htpackage kz.test.biometric
-
-import android.Manifest
-import android.annotation.SuppressLint
-import android.content.Context
-import android.content.Context.CAMERA_SERVICE
-import android.content.pm.PackageManager
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
-import android.hardware.Camera
-import android.hardware.camera2.CameraCharacteristics
-import android.hardware.camera2.CameraManager
-import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.view.Window
-import android.webkit.*
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.app.ActivityCompat
-import androidx.core.os.bundleOf
-import androidx.fragment.app.DialogFragment
-import androidx.fragment.app.FragmentManager
-import androidx.lifecycle.LifecycleOwner
+import kotlinx.coroutines.launch
 import kz.test.biometric.databinding.DialogBiometricBinding
-import org.json.JSONObject
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.URL
-
 
 //private const val BASE_URL = "https://test.biometric.kz/document?"
 private const val BASE_URL = "https://test.biometric.kz/demo/short?"
 private const val REQUEST_KEY = "biometricKey"
 private const val KEY_URL_RESULT = "keyUrlResult"
 private const val KEY_SESSION_RESULT = "keySessionResult"
+private const val KEY_EMULATOR_DETECTED = "keyEmulatorDetected"
 private const val SUCCESS = "success"
 private const val FAILURE = "failure"
 
@@ -80,6 +46,34 @@ class BiometricDialog : DialogFragment(R.layout.dialog_biometric) {
     )
     private lateinit var binding: DialogBiometricBinding
     private val token by lazy { arguments?.getString("keyToken").orEmpty() }
+
+    private val isProbablyRunningOnEmulator: Boolean by lazy {
+        return@lazy ((Build.MANUFACTURER == "Google" && Build.BRAND == "google" &&
+            ((Build.FINGERPRINT.startsWith("google/sdk_gphone_")
+                && Build.FINGERPRINT.endsWith(":user/release-keys")
+                && Build.PRODUCT.startsWith("sdk_gphone_")
+                && Build.MODEL.startsWith("sdk_gphone_"))
+                || (Build.FINGERPRINT.startsWith("google/sdk_gphone64_")
+                && (Build.FINGERPRINT.endsWith(":userdebug/dev-keys") || Build.FINGERPRINT.endsWith(
+                ":user/release-keys"
+            ))
+                && Build.PRODUCT.startsWith("sdk_gphone64_")
+                && Build.MODEL.startsWith("sdk_gphone64_"))))
+            || Build.FINGERPRINT.startsWith("generic")
+            || Build.FINGERPRINT.startsWith("unknown")
+            || Build.MODEL.contains("google_sdk")
+            || Build.MODEL.contains("Emulator")
+            || Build.MODEL.contains("Android SDK built for x86")
+            || "QC_Reference_Phone" == Build.BOARD && !"Xiaomi".equals(
+            Build.MANUFACTURER,
+            ignoreCase = true
+        )
+            || Build.MANUFACTURER.contains("Genymotion")
+            || Build.HOST.startsWith("Build")
+            || Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic")
+            || Build.PRODUCT == "google_sdk"
+            || SystemProperties.getProp("ro.kernel.qemu") == "1")
+    }
 
     override fun onStart() {
         super.onStart()
@@ -96,7 +90,7 @@ class BiometricDialog : DialogFragment(R.layout.dialog_biometric) {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = DialogBiometricBinding.inflate(layoutInflater)
         dialog?.requestWindowFeature(Window.FEATURE_NO_TITLE)
         if (!isPermissionGranted()) {
@@ -105,6 +99,21 @@ class BiometricDialog : DialogFragment(R.layout.dialog_biometric) {
             binding.webViewSetup()
         }
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        dialog?.window?.setFlags(
+            WindowManager.LayoutParams.FLAG_SECURE,
+            WindowManager.LayoutParams.FLAG_SECURE
+        )
+        super.onViewCreated(view, savedInstanceState)
+        lifecycleScope.launch {
+            if (isProbablyRunningOnEmulator) {
+                parentFragmentManager.setFragmentResult(
+                    REQUEST_KEY, bundleOf(KEY_EMULATOR_DETECTED to true)
+                )
+            }
+        }
     }
 
     private fun askPermissions() {
@@ -144,16 +153,26 @@ class BiometricDialog : DialogFragment(R.layout.dialog_biometric) {
                     if (strCurrentLine.contains("\"result\":false")) {
                         parentFragmentManager.setFragmentResult(
                             REQUEST_KEY,
-                            bundleOf(KEY_URL_RESULT to FAILURE, KEY_SESSION_RESULT to strCurrentLine)
+                            bundleOf(
+                                KEY_URL_RESULT to FAILURE,
+                                KEY_SESSION_RESULT to strCurrentLine
+                            )
                         )
                     } else if (strCurrentLine.contains("\"result\":true")) {
                         parentFragmentManager.setFragmentResult(
                             REQUEST_KEY,
-                            bundleOf(KEY_URL_RESULT to SUCCESS, KEY_SESSION_RESULT to strCurrentLine))
+                            bundleOf(
+                                KEY_URL_RESULT to SUCCESS,
+                                KEY_SESSION_RESULT to strCurrentLine
+                            )
+                        )
                     } else {
                         parentFragmentManager.setFragmentResult(
                             REQUEST_KEY,
-                            bundleOf(KEY_URL_RESULT to FAILURE, KEY_SESSION_RESULT to strCurrentLine)
+                            bundleOf(
+                                KEY_URL_RESULT to FAILURE,
+                                KEY_SESSION_RESULT to strCurrentLine
+                            )
                         )
                     }
                 } else {
@@ -164,13 +183,13 @@ class BiometricDialog : DialogFragment(R.layout.dialog_biometric) {
                         bundleOf(KEY_URL_RESULT to FAILURE, KEY_SESSION_RESULT to strCurrentLine)
                     )
                 }
-            } catch (e: Exception) { }
+            } catch (_: Exception) {
+            }
         }.start()
     }
 
     private fun extractSession(url: String): String {
-        val text = url.substring(url.indexOf("session", ignoreCase = true) + 8, url.length)
-        return text
+        return url.substring(url.indexOf("session", ignoreCase = true) + 8, url.length)
     }
 
     private var resultCount = 0
@@ -178,6 +197,28 @@ class BiometricDialog : DialogFragment(R.layout.dialog_biometric) {
     @SuppressLint("SetJavaScriptEnabled")
     private fun DialogBiometricBinding.webViewSetup() {
         webView.apply {
+            settings.javaScriptEnabled = true
+            settings.javaScriptCanOpenWindowsAutomatically = true
+            settings.domStorageEnabled = true
+            settings.allowContentAccess = true
+            settings.mediaPlaybackRequiresUserGesture = false
+
+            settings.cacheMode = WebSettings.LOAD_NO_CACHE
+            if (Build.VERSION.SDK_INT >= 19) {
+                webView.setLayerType(View.LAYER_TYPE_HARDWARE, null)
+            } else {
+                webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null)
+            }
+
+            webViewClient = object : WebViewClient() {
+                override fun shouldOverrideUrlLoading(
+                    view: WebView?,
+                    request: WebResourceRequest?
+                ): Boolean {
+                    return super.shouldOverrideUrlLoading(view, request)
+                }
+            }
+
             webChromeClient = object : WebChromeClient() {
 
                 override fun onPermissionRequest(request: PermissionRequest) {
@@ -203,23 +244,6 @@ class BiometricDialog : DialogFragment(R.layout.dialog_biometric) {
                     resultCount++
                 }
             }
-            settings.javaScriptEnabled = true
-            settings.javaScriptCanOpenWindowsAutomatically = true
-            settings.domStorageEnabled = true
-            settings.allowContentAccess = true
-            settings.mediaPlaybackRequiresUserGesture = false
-
-            settings.cacheMode = WebSettings.LOAD_NO_CACHE
-            clearCache(true)
-
-            webViewClient = object : WebViewClient() {
-                override fun shouldOverrideUrlLoading(
-                    view: WebView?,
-                    request: WebResourceRequest?
-                ): Boolean {
-                    return super.shouldOverrideUrlLoading(view, request)
-                }
-            }
             loadUrl("${BASE_URL}api_key=$token&webview=true")
         }
     }
@@ -234,6 +258,12 @@ class BiometricDialog : DialogFragment(R.layout.dialog_biometric) {
         }
     }
 
+    override fun onDestroy() {
+        binding.webView.clearCache(true)
+        binding.webView.clearHistory()
+        super.onDestroy()
+    }
+
     companion object {
 
         private val instance: BiometricDialog = BiometricDialog()
@@ -242,7 +272,8 @@ class BiometricDialog : DialogFragment(R.layout.dialog_biometric) {
             token: String,
             fragmentManager: FragmentManager,
             lifecycleOwner: LifecycleOwner,
-            onUrlChanged: OnUrlChangeListener
+            onUrlChanged: OnUrlChangeListener,
+            onEmulatorDetected: ((String) -> Unit)? = null
         ) {
             instance.apply {
                 arguments = bundleOf("keyToken" to token)
@@ -256,6 +287,10 @@ class BiometricDialog : DialogFragment(R.layout.dialog_biometric) {
                         SUCCESS -> onUrlChanged.onResultSuccess(sessionResult)
                         FAILURE -> onUrlChanged.onResultFailure(sessionResult)
                     }
+                }
+                val emulatorDetect = bundle.getBoolean(KEY_EMULATOR_DETECTED)
+                if (emulatorDetect) {
+                    onEmulatorDetected?.invoke("Emulator Detected")
                 }
                 instance.dismiss()
             }
